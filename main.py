18b6e8347f7aaa11cedcbcc92132a9a842d9b109
Okan Olgun <98118524+okanolgun@users.noreply.github.com>
2024-10-06 21:27:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import matplotlib.pyplot as plt
 import pandas_datareader as web
 import datetime as dt
 
-
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Dropout
-from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dense, Dropout, LSTM
 
+# loading data
+company = 'AAPL'
+# apple stock
+
+start = dt.datetime(2012, 1, 1)
+end = dt.datetime(2024, 10, 1)
+
+data = web.DataReader(company, 'yahoo', start, end)
